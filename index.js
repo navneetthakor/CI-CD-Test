4e40e24131b3e02223bd6685778232dba6f2eee7
navneetthakor <tnavneet8628@gmail.com>
2025-11-01 14:25:10 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,23 @@
+
+// /------------------------- importing required modules----------------------
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 5002;
+
+// --------------------------- Middleware setup ----------------------------
+// to enable cross origin resource sharing
+app.use(cors());
+
+// to parse the body of request (specifically for post requests)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// default routes
+app.get("/", (req, res) => res.json({ signal: "green" }));
+
+
+// -------------------------- Starting backend -----------------------------
+app.listen(port, () => {
+  console.log("backend is listening at port no : ", port);
+});
